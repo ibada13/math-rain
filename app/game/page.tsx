57494f2ad7f509a276e9afe09e8797a1bb2ec4d8
hunter666hz <hzhunter7@gmail.com>
2024-10-15 18:07:39 +0100

    modified:   app/game/page.tsx

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -5,23 +5,36 @@ export default function Game() {
 
     useEffect(() => { 
         const rains = document.querySelectorAll("#rains")
+        const targetdiv = document.getElementById("target") as HTMLDivElement
+        const targetY = targetdiv.getBoundingClientRect().top
+        console.log(targetY)
         gsap.fromTo(rains, {
             y:0,
        }, {
-            y: window.innerHeight,
+            y: targetY -100   ,
             duration: 10,
            
         })
     },[])
 
     return (
-        <div className="h-screen w-screen flex items-start justify-around   ">
+        <div className="bg-pink-500 h-screen w-screen flex flex-col justify-between    ">
+            <div className="flex-grow flex items-start justify-around">
+
             <div id="rains" className="bg-blue-500 rounded-md p-2 m-2">rains</div>
             <div id="rains" className="bg-blue-500 rounded-md p-2 m-2">rains</div>
             <div id="rains" className="bg-blue-500 rounded-md p-2 m-2">rains</div>
             <div id="rains" className="bg-blue-500 rounded-md p-2 m-2">rains</div>
             <div id="rains" className="bg-blue-500 rounded-md p-2 m-2">rains</div>
-            <div className="self-end w-full ">hello there hahaha im in danger</div>
+            </div>
+            <div>
+                
+                <div id="target" className="p-2 bg-red-500 flex justify-center items-center">
+                    <p className="text-2xl">
+                    hello there hahaha im in danger
+                    </p>
+                </div>
+            </div>
         </div>
     )
 } 
